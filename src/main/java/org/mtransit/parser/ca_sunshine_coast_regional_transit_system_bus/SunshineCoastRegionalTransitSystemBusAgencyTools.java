d698f4e35bd2c3f26cb552e55c76fe74278b77c6
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-01 09:09:12 -0500

    GTFS Direction Finder > enable

--- src/main/java/org/mtransit/parser/ca_sunshine_coast_regional_transit_system_bus/SunshineCoastRegionalTransitSystemBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_sunshine_coast_regional_transit_system_bus/SunshineCoastRegionalTransitSystemBusAgencyTools.java
@@ -5,29 +5,18 @@ import org.jetbrains.annotations.Nullable;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Pair;
-import org.mtransit.parser.SplitUtils;
-import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.StringUtils;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
-import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MDirectionType;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.regex.Pattern;
 
 // https://www.bctransit.com/open-data
@@ -148,136 +137,17 @@ public class SunshineCoastRegionalTransitSystemBusAgencyTools extends DefaultAge
 		return super.getRouteColor(gRoute);
 	}
 
-	private static final HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
-
-	static {
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
-		//noinspection deprecation
-		map2.put(2L, new RouteTripSpec(2L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.EAST.getId(), // Trail Bay Mall
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.WEST.getId()) // Mason & Norwest
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList(//
-								"170006", // Eastbound Cowrie at Trail
-								"170219", // ==
-								"170222", // !=
-								"118024", // !=
-								"118031", // !=
-								"170223", // ==
-								"170335" // Southbound Mason at Northwest Bay
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList(//
-								"170335", // Southbound Mason at Northwest Bay
-								"170232", // ++
-								"170006" // Eastbound Cowrie at Trail
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(3L, new RouteTripSpec(3L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), // Sechelt Arena
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) // Trail Bay Mall
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(//
-								"170006", // Eastbound Cowrie at Trail
-								"170249", // Northbound Fairway at Shoal
-								"118017" // Eastbound Harbour Way at Fairway
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(//
-								"118017", // Eastbound Harbour Way at Fairway
-								"170241", // Westbound Marine at Trail
-								"170006" // Eastbound Cowrie at Trail
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(4L, new RouteTripSpec(4L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.EAST.getId(), // Trail Bay Mall
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.WEST.getId()) // Halfmoon Bay / Secret Cove
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList(//
-								"170999", // Northbound Redrooffs at Sunshine Coast Highway
-								"170317", //
-								"170006" // Eastbound Cowrie at Trail
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList(//
-								"170006", // Eastbound Cowrie at Trail
-								"170223", // == Westbound Norwest Bay at Derby
-								"118000", // != Westbound Norwest Bay at Nickerson
-								"118004", // != Southbound Emerson at Lloyd
-								"170224", // != Southbound Nickerson at Norwest Bay
-								"170225", // != Eastbound Norwest Bay at Winter
-								"170226", // == Southbound McCourt at Norwest Bay
-								"170290", //
-								"170999" // Northbound Redrooffs at Sunshine Coast Highway
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(90L, new RouteTripSpec(90L, //
-				0, MTrip.HEADSIGN_TYPE_STRING, "Sechelt", //
-				1, MTrip.HEADSIGN_TYPE_STRING, "Langdale Ferry") //
-				.addTripSort(0, //
-						Arrays.asList(//
-								"170101", // Westbound Langdale Ferry Terminal at Sunshine Coast
-								"170106", // ==
-								"118025", // !=
-								"170128",// !=
-								"170107", // !=
-								"170119", // !=
-								"170131", // ==
-								"170003" // Eastbound Cowrie at Trail
-						)) //
-				.addTripSort(1, //
-						Arrays.asList(//
-								"170003", // Eastbound Cowrie at Trail
-								"170072", // ==
-								"170073", // !=
-								"170098", // !=
-								"170120", // !=
-								"118023", // !=
-								"170099", // ==
-								"170101" // Westbound Langdale Ferry Terminal at Sunshine Coast
-						)) //
-				.compileBothTripSort());
-		ALL_ROUTE_TRIPS2 = map2;
-	}
-
-	@Override
-	public int compareEarly(long routeId, @NotNull List<MTripStop> list1, @NotNull List<MTripStop> list2, @NotNull MTripStop ts1, @NotNull MTripStop ts2, @NotNull GStop ts1GStop, @NotNull GStop ts2GStop) {
-		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
-		}
-		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
-	}
-
-	@NotNull
-	@Override
-	public ArrayList<MTrip> splitTrip(@NotNull MRoute mRoute, @Nullable GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
-		}
-		return super.splitTrip(mRoute, gTrip, gtfs);
-	}
-
-	@NotNull
-	@Override
-	public Pair<Long[], Integer[]> splitTripStop(@NotNull MRoute mRoute, @NotNull GTrip gTrip, @NotNull GTripStop gTripStop, @NotNull ArrayList<MTrip> splitTrips, @NotNull GSpec routeGTFS) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
-		}
-		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
-	}
-
 	@Override
 	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return; // split
-		}
 		mTrip.setHeadsignString(
 				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
 				gTrip.getDirectionIdOrDefault()
 		);
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
 	}
 
 	private static final Pattern EXPRESS_ = CleanUtils.cleanWords("express");
